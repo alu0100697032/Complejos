d9a6084820e276a5d728c35a047a8a8c1685bdb4
Victor Hernandez Perez <alu0100697032.edu.es>
2014-10-16 15:02:04 +0000

    Added test for scalar multiplication

--- test/tests.rb
+++ test/tests.rb
@@ -3,8 +3,10 @@ require "test/unit"
  
 class TestComplejos < Test::Unit::TestCase
  
-  def test_simple
+  def test_to_s
     assert_equal("2 +2i", Complejo.new(2,2).to_s )
+    assert_equal("2 -2i", Complejo.new(2,-2).to_s )
+    assert_equal(4, (Complejo.new(2,2)*2).real)
   end
- 
+  
 end
