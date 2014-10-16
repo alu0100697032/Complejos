92fe41deaf4034486a746296c6d36b13ee6683cf
Victor Hernandez Perez <alu0100697032.edu.es>
2014-10-16 12:24:39 +0000

    Added all operations

--- complejos.rb
+++ complejos.rb
@@ -1,5 +1,6 @@
 #clase para representar los numeros complejos
 class Complejo
+    attr_reader :real,:imaginaria #define los metodos de acceso a nuestros atributos
     #constructor de la clase
     def initialize(real, imaginaria)
         #atributos
@@ -8,19 +9,29 @@ class Complejo
     end
     #representa el numero complejo 
     def to_s
-        if @imaginaria.to_i > 0
-            puts "#{@real} +#{@imaginaria}i"
+        if @imaginaria.to_f > 0
+            "#{@real} +#{@imaginaria}i"
         else 
-            puts "#{@real} #{@imaginaria}i"
+            "#{@real} #{@imaginaria}i"
         end    
     end
+    #multiplicacion
+    def *(escalar)
+        return Complejo.new(@real*escalar, @imaginaria*escalar) if escalar.is_a? Numeric
+        return Complejo.new(@real*escalar.real-@imaginaria*escalar.imaginaria,
+        @real*escalar.imaginaria+@imaginaria*escalar.real) if escalar.is_a? Complejo
+    end
+    #division
+    def /(other)
+        return Complejo.new(((@real*other.real)+(@imaginaria*other.imaginaria))/((other.real*other.real)+(other.imaginaria*other.imaginaria)),
+        (((@imaginaria*other.real)-(@real*other.imaginaria))/((other.real*other.real)+(other.imaginaria*other.imaginaria)))) 
+    end
+    #suma
+    def +(other)
+        return Complejo.new(@real+other.real, @imaginaria+other.imaginaria)
+    end
+    #resta
+    def -(other)
+        return Complejo.new(@real-other.real, @imaginaria-other.imaginaria)
+    end
 end
-
-#suma de dos complejos 
-=begin
-def suma(a, b)
-    #aux = Complejo.new(@real+c2.real, @imaginaria+c2.imaginaria)
-    puts "Suma: #{a.real + b.real}" 
-end
-=end
-#suma(c1, c2)
--- main.rb
+++ main.rb
@@ -1,7 +1,31 @@
+#! /usr/local/rvm/rubies/ruby-2.1.1/bin/ruby
+# encoding: UTF-8
 load 'complejos.rb'
 
 #instancia de un complejo y representacion del mismo
-c1 = Complejo.new(5,3)
+c1 = Complejo.new(5,1)
 c2 = Complejo.new(2,4)
-c1.to_s
-c2.to_s
+puts "Complejo 1: "  
+puts c1.to_s
+puts "Complejo 2: " 
+puts c2.to_s
+#producto escalar 
+puts "Producto escalar:"
+puts '5x' + '('+ c1.to_s + ') = ' 
+puts c1*5
+#multiplicacion
+puts "Multiplicacion:"
+puts "Complejo 1 * Complejo 2 = "
+puts c1*c2
+#division 
+puts "Division:"
+puts "Complejo 1 / Complejo 2 = "
+puts c1/c2
+#suma
+puts "Suma:"
+puts "Complejo 1 + Complejo 2 = "
+puts c1+c2
+#resta
+puts "Resta:"
+puts "Complejo 1 - Complejo 2 = "
+puts c1-c2
